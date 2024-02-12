eb406e1ae1949a24c85814f3bd8ffb842884b3a8
benmusson <ben.d.musson@gmail.com>
2024-02-11 22:07:34 -0500

    move tasks to plugin

--- modules/example-perspective-component/gateway/build.gradle.kts
+++ modules/example-perspective-component/gateway/build.gradle.kts
@@ -2,31 +2,9 @@ plugins {
     id("embr.ignition-module-library-conventions")
 }
 
-val resourcesPath = "../src/main/resources/web"
-val jsBundles: Configuration by configurations.creating {
-    isCanBeConsumed = false
-}
-
 dependencies {
     compileOnly(libs.bundles.gateway)
     compileOnly(libs.bundles.perspectiveFateway)
     compileOnly(projects.common)
-    compileOnly(projects.web)
-    jsBundles(project(path = ":web", configuration = "example_perspective_component"))
-}
-
-tasks {
-    named("processResources") {
-        dependsOn("collectWebResources")
-    }
-    register<Copy>("collectWebResources") {
-        group = "build"
-        val jsBundleFiles: FileCollection = jsBundles
-        from(jsBundleFiles)
-        into(layout.buildDirectory.dir(resourcesPath))
-    }
-
-    named("clean") {
-        doFirst { delete(layout.buildDirectory.dir(resourcesPath)) }
-    }
+    modlWebResource(project(path = ":web", configuration = "example_perspective_component"))
 }
