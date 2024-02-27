bd4247eef6106e074e1abc4e0f8cb8769beb30e9
benmusson <ben.d.musson@gmail.com>
2024-02-27 01:08:53 -0500

    only overwrite snapshot builds

--- buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
         releaseName("${project.name}-${version}")
         generateReleaseNotes(true)
         releaseAssets.from(tasks.signModule.get().signed)
-        overwrite.set(true)
+        overwrite.set(version.toString().contains("-SNAPSHOT"))
     }
 }
 
