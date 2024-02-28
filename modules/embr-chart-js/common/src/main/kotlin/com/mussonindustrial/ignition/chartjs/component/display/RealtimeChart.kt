40b4e3fa95edce47a6ce69f406b44f4ca55d34b4
benmusson <ben.d.musson@gmail.com>
2024-02-28 00:58:29 -0500

    start realtime chart dev

--- modules/embr-chart-js/common/src/main/kotlin/com/mussonindustrial/ignition/chartjs/component/display/RealtimeChart.kt
+++ modules/embr-chart-js/common/src/main/kotlin/com/mussonindustrial/ignition/chartjs/component/display/RealtimeChart.kt
@@ -7,19 +7,19 @@ import com.mussonindustrial.ignition.chartjs.Components
 import com.mussonindustrial.ignition.chartjs.Components.COMPONENT_CATEGORY
 import com.mussonindustrial.ignition.chartjs.Meta.MODULE_ID
 
-class TagHistoryChart {
+class RealtimeChart {
     companion object {
-        var COMPONENT_ID: String = "mussonindustrial.chart.chart-js.tag-history-chart"
+        var COMPONENT_ID: String = "mussonindustrial.chart.chart-js.realtime"
         var SCHEMA: JsonSchema = JsonSchema.parse(Components::class.java.getResourceAsStream("/chart-js.props.json"))
         var DESCRIPTOR: ComponentDescriptor = ComponentDescriptorImpl.ComponentBuilder.newBuilder()
             .setPaletteCategory(COMPONENT_CATEGORY)
             .setId(COMPONENT_ID)
             .setModuleId(MODULE_ID)
             .setSchema(SCHEMA)
-            .setName("Tag History Chart")
+            .setName("Realtime Chart")
 //            .setIcon(ImageIcon(Components::class.java.getResource("/icons/simplecomponent.png")))
-            .addPaletteEntry("", "Tag History Chart", "Tag History Chart", null, null)
-            .setDefaultMetaName("Tag History Chart")
+            .addPaletteEntry("", "Realtime Chart", "Realtime Chart", null, null)
+            .setDefaultMetaName("Realtime Chart")
             .setResources(Components.BROWSER_RESOURCES)
             .build()
     }
