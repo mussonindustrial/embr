b38b44cbe02a405ae159b97d4ca568ab1ed344c1
benmusson <ben.d.musson@gmail.com>
2024-05-16 03:29:12 -0400

    add beta support for scriptable properties

--- js/packages/embr-chart-js/src/components/BaseChartComponent.tsx
+++ js/packages/embr-chart-js/src/components/BaseChartComponent.tsx
@@ -7,10 +7,42 @@ import {
     SizeObject,
 } from '@inductiveautomation/perspective-client'
 import { Chart, ChartProps } from 'react-chartjs-2'
+import { ChartTypeRegistry, ScriptableContext } from 'chart.js'
 
 export const COMPONENT_TYPE = 'mussonindustrial.chart.chart-js'
 
+export type OptionScript = (
+    context: ScriptableContext<keyof ChartTypeRegistry>,
+    options: object
+) => unknown
+
+function recursiveMap<T>(obj: T, callback: (obj: unknown) => unknown): unknown {
+    if (Array.isArray(obj)) return obj.map((v) => recursiveMap(v, callback))
+    if (obj && typeof obj === 'object')
+        return Object.fromEntries(
+            Object.entries(obj).map(([k, v]) => [k, recursiveMap(v, callback)])
+        )
+    return callback(obj)
+}
+
+function transformFunctionString(script: string): OptionScript | string {
+    if (script.includes('return ')) {
+        return new Function('context', 'options', script) as OptionScript
+    }
+    return script
+}
+
+function transformProps(props: ChartProps) {
+    return recursiveMap(props, (object) => {
+        if (typeof object === 'string') {
+            return transformFunctionString(object)
+        }
+        return object
+    })
+}
+
 export function BaseChartComponent(props: ComponentProps<ChartProps>) {
+    props.props = transformProps(props.props) as ChartProps
     return (
         <div {...props.emit()}>
             <Chart
