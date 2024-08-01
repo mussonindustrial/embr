12e95dec738ab11c74cc43717daff5f8e7bb48c8
benmusson <ben.d.musson@gmail.com>
2024-08-01 17:07:10 -0400

    add release task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.gradle.node.npm.task.NpxTask
+
 plugins {
     base
+    id("com.github.node-gradle.node")
 }
 
 repositories {
@@ -14,4 +17,24 @@ tasks.register("buildModules") {
     group = "build"
     dependsOn(":modules:embr-charts:build")
     dependsOn(":modules:embr-tag-stream:build")
+}
+
+val changesetVersion = tasks.register<NpxTask>("changesetVersion") {
+    group = "changesets"
+    dependsOn(tasks.build)
+    command.set("changeset")
+    args.set(listOf("version"))
+}
+
+val changesetPublish = tasks.register<NpxTask>("changesetPublish") {
+    group = "changesets"
+    dependsOn(tasks.build)
+    mustRunAfter(changesetVersion)
+    command.set("changeset")
+    args.set(listOf("publish"))
+}
+
+val release = tasks.register("release") {
+    group = "publishing"
+    dependsOn(changesetVersion, changesetPublish)
 }
