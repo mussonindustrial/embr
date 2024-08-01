5f7c6234d329117f5253cb81be2c02ebb859b218
benmusson <ben.d.musson@gmail.com>
2024-08-01 18:02:46 -0400

    buildSrc simplification

--- buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
@@ -4,7 +4,6 @@ import java.util.*
 plugins {
     id("embr.kotlin-library-conventions")
     id("io.ia.sdk.modl")
-    id("com.github.breadmoirai.github-release")
 }
 
 allprojects {
@@ -14,24 +13,6 @@ allprojects {
 fun buildTime(): String { return SimpleDateFormat("yyyyMMddHH").format(Date()) }
 ignitionModule {
     moduleVersion.set("${version}.${buildTime()}")
-}
-
-afterEvaluate {
-    githubRelease {
-        token(project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN"))
-        owner("mussonindustrial")
-        repo("embr")
-        targetCommitish("main")
-        tagName("${project.name}-${version}")
-        releaseName("${project.name}-${version}")
-        generateReleaseNotes(true)
-        releaseAssets.from(tasks.signModule.get().signed)
-        overwrite.set(true)
-    }
-}
-
-tasks.githubRelease {
-    dependsOn(tasks.build)
 }
 
 tasks.deployModl {
