6e48fe66d8365bc27dccc01413fd8007d4d38a5e
Ben Musson <58087114+benmusson@users.noreply.github.com>
2024-08-02 18:16:57 -0400

    Fix/gha syntax2 (#145)
    * traverse folders
    
    * traverse folders (correctly)
    
    * Save build artifacts
    
    * Create bright-phones-hear.md
    
    * Add task to compile and zip all modules

--- buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/embr.ignition-module-conventions.gradle.kts
@@ -18,3 +18,24 @@ ignitionModule {
 tasks.deployModl {
     hostGateway = "http://localhost:8088"
 }
+
+
+val releaseFiles: Configuration = configurations.create("releaseFiles") {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+}
+
+// I don't understand why this has to be done way.
+// I'd like to use the output file of the signModule task, but any way I try gradle throws:
+// > Querying the mapped value of task ':modules:embr-charts:signModule' property 'unsigned'
+// before task ':modules:embr-charts:zipModule' has completed is not supported
+val fileName = ignitionModule.fileName.flatMap { project.provider { it } }
+val signedModule = project.provider { file("${projectDir}/build/${fileName.get()}") }
+
+afterEvaluate {
+    artifacts {
+        add(releaseFiles.name, signedModule) {
+            builtBy(tasks.signModule)
+        }
+    }
+}
