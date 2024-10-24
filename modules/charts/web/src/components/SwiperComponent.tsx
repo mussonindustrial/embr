a5c473a7a98315f4eee1b5d97cefb8ea1601a749
Ben Musson <58087114+benmusson@users.noreply.github.com>
2024-10-23 20:43:11 -0400

    reorganize, use resize detector

--- modules/charts/web/src/components/SwiperComponent.tsx
+++ modules/charts/web/src/components/SwiperComponent.tsx
@@ -1,41 +1,43 @@
 import React, { useRef } from 'react'
 import {
-    ComponentMeta,
-    ComponentProps,
-    JsObject,
-    PageStore,
-    PComponent,
-    PlainObject,
-    PropertyTree,
-    SizeObject,
-    StyleObject,
-    View,
+  ClientStore,
+  ComponentMeta,
+  ComponentProps,
+  JsObject,
+  PageStore,
+  PComponent,
+  PlainObject,
+  PropertyTree,
+  ReactResizeDetector,
+  SizeObject,
+  StyleObject,
+  View,
 } from '@inductiveautomation/perspective-client'
 
-import { Swiper, SwiperSlide, SwiperProps, SwiperRef } from 'swiper/react'
+import { Swiper, SwiperSlide, SwiperProps, SwiperRef, useSwiper, useSwiperSlide } from 'swiper/react'
 import { Virtual,
-    Keyboard,
-    Mousewheel,
-    Navigation,
-    Pagination,
-    Scrollbar,
-    Parallax,
-    FreeMode,
-    Grid,
-    Manipulation,
-    Zoom,
-    Controller,
-    A11y,
-    History,
-    HashNavigation,
-    Autoplay,
-    EffectFade,
-    EffectCube,
-    EffectFlip,
-    EffectCoverflow,
-    EffectCards,
-    EffectCreative,
-    Thumbs 
+  Keyboard,
+  Mousewheel,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Parallax,
+  FreeMode,
+  Grid,
+  Manipulation,
+  Zoom,
+  Controller,
+  A11y,
+  History,
+  HashNavigation,
+  Autoplay,
+  EffectFade,
+  EffectCube,
+  EffectFlip,
+  EffectCoverflow,
+  EffectCards,
+  EffectCreative,
+  Thumbs 
 } from 'swiper/modules'
 
 import 'swiper/css/bundle';
@@ -45,130 +47,141 @@ import { getScriptTransform } from '../util';
 
 const COMPONENT_TYPE = 'embr.chart.swiper'
 
+const EnabledSwiperModules = [
+  Virtual,
+  Keyboard,
+  Mousewheel,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Parallax,
+  FreeMode,
+  Grid,
+  Manipulation,
+  Zoom,
+  Controller,
+  A11y,
+  History,
+  HashNavigation,
+  Autoplay,
+  EffectFade,
+  EffectCube,
+  EffectFlip,
+  EffectCoverflow,
+  EffectCards,
+  EffectCreative,
+  Thumbs
+]
+
 type EmbeddedViewProps = {
-    viewPath: string
-    viewParams: JsObject
-    viewStyle: StyleObject
-    slideStyle: StyleObject
+  viewPath: string
+  viewParams: JsObject
+  viewStyle: StyleObject
+  slideStyle: StyleObject
+  useDefaultHeight: boolean
+  useDefaultMinHeight: boolean
+  useDefaultMinWidth: boolean
+  useDefaultWidth: boolean
 }
     
 type SwiperComponentProps = {
-    viewPath?: string
-    viewParams?: JsObject
-    viewStyle: StyleObject
-    slideStyle: StyleObject
-    instances: EmbeddedViewProps[]
-    settings?: SwiperProps
-    style?: StyleObject
+  viewPath?: string
+  viewParams?: JsObject
+  viewStyle: StyleObject
+  slideStyle: StyleObject
+  instances: EmbeddedViewProps[]
+  settings?: SwiperProps
+  style?: StyleObject
 }
-type SlideData = {
-    isActive: boolean;
-    isVisible: boolean;
-    isPrev: boolean;
-    isNext: boolean;
-  }
 
 function getMountMountPath(props: ComponentProps<PlainObject>, id: any) {
-    return `${props.store.viewMountPath}$${props.store.addressPathString}[${id}]`
+  return `${props.store.viewMountPath}$${props.store.addressPathString}[${id}]`
 }
 
 function resolve(inputs: Array<unknown>) {
-    let value: unknown
-    for (let i = 0; i < inputs.length; i++) {
-        value = inputs[i]
-        if (value === undefined) {
-            continue;
-        }
-        if (value !== undefined) {
-            return value as any;
-        }
+  let value: unknown
+  for (let i = 0; i < inputs.length; i++) {
+    value = inputs[i]
+    if (value === undefined) {
+      continue;
     }
+    if (value !== undefined) {
+      return value as any;
+    }
+  }
 }
 
-function resolveSlideProps(props: SwiperComponentProps, slideIndex: number): EmbeddedViewProps {
-    const slideProps = props.instances[slideIndex]
+function resolveViewProps(props: SwiperComponentProps, slideIndex: number): EmbeddedViewProps {
+  const view = props.instances[slideIndex]
 
-    return {
-        viewPath: resolve([slideProps.viewPath, props.viewPath]),
-        viewParams: {
-            ...props.viewParams,
-            ...slideProps.viewParams,
-            index: slideIndex
-        },
-        viewStyle: {
-            ...props.viewStyle,
-            ...slideProps.viewStyle
-        },
-        slideStyle: {
-            ...props.slideStyle,
-            ...slideProps.slideStyle
-        }
-    }
+  return {
+      viewPath: resolve([view.viewPath, props.viewPath]),
+      viewParams: {
+        ...props.viewParams,
+        ...view.viewParams,
+        index: slideIndex
+      },
+      viewStyle: {
+        ...props.viewStyle,
+        ...view.viewStyle
+      },
+      slideStyle: {
+        ...props.slideStyle,
+        ...view.slideStyle
+      },
+      useDefaultHeight: view.useDefaultHeight,
+      useDefaultMinHeight: view.useDefaultMinHeight,
+      useDefaultMinWidth: view.useDefaultMinWidth,
+      useDefaultWidth: view.useDefaultWidth
+  }
 }
 
+function EmbeddedSlideView(props: {store: ClientStore, mountPath: string, view: EmbeddedViewProps}) {
+  const swiper = useSwiper()
+  const slide = useSwiperSlide()
 
-function EmbeddedSlide(props: {parent: ComponentProps<SwiperComponentProps>, index: number, slideData?: SlideData}) {
-    const mountPath = getMountMountPath(props.parent, props.index)
-    const viewProps = resolveSlideProps(props.parent.props, props.index)
-
-    return (
-        <View
-            key={PageStore.instanceKeyFor(viewProps.viewPath, mountPath)}
-            store={props.parent.store.view.page.parent}
-            mountPath={mountPath}
-            resourcePath={viewProps.viewPath}
-            params={{
-                ...viewProps.viewParams,
-                index: props.index,
-                slideData: props.slideData,
-            }}
-            rootStyle={{
-                ...viewProps.viewStyle,
-                height: '100%',
-                width: '100%'
-            }}
-        />
-    )
+  return (
+    <>        
+      <View
+        key={PageStore.instanceKeyFor(props.view.viewPath, props.mountPath)}
+        store={props.store}
+        mountPath={props.mountPath}
+        resourcePath={props.view.viewPath}
+        useDefaultHeight={props.view.useDefaultHeight}
+        useDefaultMinHeight={props.view.useDefaultMinHeight}
+        useDefaultMinWidth={props.view.useDefaultMinWidth}
+        useDefaultWidth={props.view.useDefaultWidth}
+        params={{
+          ...props.view.viewParams,
+          slideData: slide,
+        }}
+        rootStyle={{
+          width: props.view.useDefaultWidth ? undefined : '100%',
+          height: props.view.useDefaultHeight ? undefined : '100%',
+          ...props.view.viewStyle,
+        }}
+      />
+      <ReactResizeDetector
+        onResize={() => { console.log('resized'); swiper.update(); }}
+        handleWidth={true}
+        handleHeight={true}
+      />
+    </>
+  )
 }
 
 function extractSettings(props: SwiperComponentProps) {
-    const settings = props.settings
-    unset(props, 'settings')
-    return { props, settings }
+  const settings = props.settings
+  unset(props, 'settings')
+  return { props, settings }
 }
 
 function installSettings(
-    props: SwiperComponentProps,
-    settings: SwiperProps
+  props: SwiperComponentProps,
+  settings: SwiperProps
 ) {
-    props.settings = settings
+  props.settings = settings
 }
-
-const EnabledSwiperModules = [
-    Virtual,
-    Keyboard,
-    Mousewheel,
-    Navigation,
-    Pagination,
-    Scrollbar,
-    Parallax,
-    FreeMode,
-    Grid,
-    Manipulation,
-    Zoom,
-    Controller,
-    A11y,
-    History,
-    HashNavigation,
-    Autoplay,
-    EffectFade,
-    EffectCube,
-    EffectFlip,
-    EffectCoverflow,
-    EffectCards,
-    EffectCreative,
-    Thumbs
-]
 
 export function SwiperComponent(props: ComponentProps<SwiperComponentProps>) {
 
@@ -176,69 +189,68 @@ export function SwiperComponent(props: ComponentProps<SwiperComponentProps>) {
 
     const { props: transformedProps, settings } = extractSettings(props.props)
     const transformedSettings = transformProps(settings, [
-        getScriptTransform({ self: props, client: window.__client })
+      getScriptTransform({ self: props, client: window.__client })
     ]) as SwiperProps
     installSettings(transformedProps, transformedSettings)
 
-    const slides = () => transformedProps.instances.map((instance, index) => {
-        const slideStyle = {
-            ...transformedProps.slideStyle,
-            ...instance.slideStyle
-        }
+    const slides = () => transformedProps.instances.map((_, index) => {
 
-        return <SwiperSlide style={slideStyle}>
-            {(slideData) => {
-                return <EmbeddedSlide 
-                    parent={props} 
-                    index={index} 
-                    slideData={slideData}
-                />
-            }}
+      const mountPath = getMountMountPath(props, index)
+      const viewProps = resolveViewProps(transformedProps, index)
+      viewProps.viewParams.index = index
+
+      return (
+        <SwiperSlide style={viewProps.slideStyle}>
+          <EmbeddedSlideView 
+            store={props.store.view.page.parent} 
+            view={viewProps}
+            mountPath={mountPath}
+          />
         </SwiperSlide>
+      )
     })
 
     return (
-        <div { ...props.emit() }>
-            <Swiper
-                ref={swiperRef}
-                modules={EnabledSwiperModules}
-                { ...transformedProps.settings }
-                observer={true}
-                observeSlideChildren={true}
-                style={{ height: '100%', width: '100%' }}
-            >
-            {...slides()}
-            </Swiper>
-        </div>
-
+      <div { ...props.emit() }>
+        <Swiper
+          ref={swiperRef}
+          modules={EnabledSwiperModules}
+          { ...transformedProps.settings }
+          observer={true}
+          observeSlideChildren={true}
+          style={{ height: '100%', width: '100%' }}
+        >
+        {...slides()}
+        </Swiper>
+      </div>
     )
 }
 
 export class SwiperComponentMeta implements ComponentMeta {
-    getComponentType(): string {
-        return COMPONENT_TYPE
-    }
+  getComponentType(): string {
+    return COMPONENT_TYPE
+  }
 
-    getDefaultSize(): SizeObject {
-        return {
-            width: 300,
-            height: 300,
-        }
+  getDefaultSize(): SizeObject {
+    return {
+      width: 300,
+      height: 300,
     }
+  }
 
-    getPropsReducer(tree: PropertyTree): SwiperProps {
-        return {
-            viewPath: tree.read('viewPath'),
-            viewParams: tree.read('viewParams'),
-            elementStyle: tree.read('elementStyle', {}),
-            slideStyle: tree.read('slideStyle', {}),
-            instances: tree.read('instances', []),
-            settings: tree.read('settings', {}),
-            style: tree.read('style', {}),
-        } as never
-    }
+  getPropsReducer(tree: PropertyTree): SwiperProps {
+    return {
+      viewPath: tree.read('viewPath'),
+      viewParams: tree.read('viewParams'),
+      elementStyle: tree.read('elementStyle', {}),
+      slideStyle: tree.read('slideStyle', {}),
+      instances: tree.read('instances', []),
+      settings: tree.read('settings', {}),
+      style: tree.read('style', {}),
+    } as never
+  }
 
-    getViewComponent(): PComponent {
-        return SwiperComponent as PComponent
-    }
+  getViewComponent(): PComponent {
+    return SwiperComponent as PComponent
+  }
 }
