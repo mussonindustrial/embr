1eab8ca3fe4d387729b96a062be209bdc6302ad9
Ben Musson <ben.d.musson@gmail.com>
2025-09-22 19:49:20 -0400

    linting

--- libraries/core/gateway/src/main/kotlin/com/mussonindustrial/embr/gateway/secrets/SecretsUtil.kt
+++ libraries/core/gateway/src/main/kotlin/com/mussonindustrial/embr/gateway/secrets/SecretsUtil.kt
@@ -4,6 +4,6 @@ import com.inductiveautomation.ignition.gateway.model.GatewayContext
 import com.inductiveautomation.ignition.gateway.secrets.Secret
 import com.inductiveautomation.ignition.gateway.secrets.SecretConfig
 
-fun GatewayContext.getAsString(secret: SecretConfig) : String {
+fun GatewayContext.getAsString(secret: SecretConfig): String {
     return Secret.create(this, secret).plaintext.asString
 }
