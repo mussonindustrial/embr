0a98ae5d4416b244ca608655292d4b708571c250
Ben Musson <ben.d.musson@gmail.com>
2026-01-16 14:54:44 -0500

    add support for parameterized types

--- libraries/core/common/src/main/kotlin/com/mussonindustrial/embr/common/scripting/PyArgOverload.kt
+++ libraries/core/common/src/main/kotlin/com/mussonindustrial/embr/common/scripting/PyArgOverload.kt
@@ -2,7 +2,12 @@ package com.mussonindustrial.embr.common.scripting
 
 import com.inductiveautomation.ignition.common.TypeUtilities
 import com.inductiveautomation.ignition.common.script.PyArgParser
+import java.lang.reflect.GenericArrayType
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import java.lang.reflect.WildcardType
 import kotlin.jvm.optionals.getOrNull
+import kotlin.reflect.KType
 import kotlin.reflect.javaType
 import org.python.core.Py
 import org.python.core.PyObject
@@ -12,7 +17,6 @@ class PyArgOverload(
     private val functions: Map<FunctionSignature, (args: Map<String, Any?>) -> Any?>,
 ) {
 
-    @OptIn(ExperimentalStdlibApi::class)
     fun call(args: Array<PyObject>, keywords: Array<String>): Any? {
         val signatures = functions.keys.flatMap { signature -> signature.parameters }.toSet()
 
@@ -21,7 +25,7 @@ class PyArgOverload(
                 args,
                 keywords,
                 signatures.map { it.name }.toTypedArray(),
-                signatures.map { it.type.javaType as Class<*> }.toTypedArray(),
+                signatures.map { it.type.rawJavaClass() }.toTypedArray(),
                 this.name,
             )
 
@@ -41,10 +45,7 @@ class PyArgOverload(
                                 argParser.getPyObject(it.name).getOrNull()
                                     ?: return@associateBy null
                             val jValue = TypeUtilities.pyToJava(pyValue)
-                            return@associateBy TypeUtilities.coerce(
-                                jValue,
-                                it.type.javaType as Class<*>,
-                            )
+                            return@associateBy TypeUtilities.coerce(jValue, it.type.rawJavaClass())
                         },
                     )
                 )
@@ -59,4 +60,26 @@ class PyArgOverload(
             "No matching function signature found for '$name'. Valid signatures include: $validSignatures"
         throw Py.TypeError(message)
     }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun KType.rawJavaClass(): Class<*> {
+        return extractClass(this.javaType)
+    }
+
+    private fun extractClass(type: Type): Class<*> =
+        when (type) {
+            is Class<*> -> type
+
+            is ParameterizedType -> extractClass(type.rawType)
+
+            is GenericArrayType -> {
+                val component = extractClass(type.genericComponentType)
+                java.lang.reflect.Array.newInstance(component, 0).javaClass
+            }
+
+            is WildcardType -> extractClass(type.upperBounds.first())
+
+            else ->
+                throw IllegalArgumentException("Unsupported type: $type (${type::class.java.name})")
+        }
 }
