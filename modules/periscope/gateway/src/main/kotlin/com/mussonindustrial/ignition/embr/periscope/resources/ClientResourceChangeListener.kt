9bae067439cb4f5efa2a75082f59c936ef073748
Ben Musson <ben.d.musson@gmail.com>
2026-05-22 17:35:04 -0400

    steal notifier from 8.3

--- modules/periscope/gateway/src/main/kotlin/com/mussonindustrial/ignition/embr/periscope/resources/ClientResourceChangeListener.kt
+++ modules/periscope/gateway/src/main/kotlin/com/mussonindustrial/ignition/embr/periscope/resources/ClientResourceChangeListener.kt
@@ -1,9 +1,7 @@
 package com.mussonindustrial.ignition.embr.periscope.resources
 
-import com.inductiveautomation.ignition.common.gson.JsonObject
+import com.inductiveautomation.ignition.common.model.ApplicationScope
 import com.inductiveautomation.ignition.common.project.RuntimeProject
-import com.inductiveautomation.ignition.common.project.resource.ProjectResource
-import com.inductiveautomation.ignition.common.project.resource.ProjectResourceId
 import com.inductiveautomation.ignition.gateway.project.ProjectLifecycle
 import com.inductiveautomation.ignition.gateway.project.ProjectLifecycleFactory
 import com.inductiveautomation.ignition.gateway.project.ProjectManager
@@ -14,41 +12,13 @@ class ClientResourceChangeListener(
     projectManager: ProjectManager,
 ) : ProjectLifecycleFactory<ProjectLifecycle>(projectManager) {
 
-    val filter: ResourceFilter =
-        ResourceFilter.newBuilder().addResourceType(ClientResource.type).build()
-
-    override fun createProjectLifecycle(project: RuntimeProject): Notifier {
-        return Notifier(project)
-    }
+    val filter = ResourceFilter(ApplicationScope.ALL, listOf(ClientResource.type))
 
     override fun getResourceFilter(): ResourceFilter {
         return filter
     }
 
-    inner class Notifier(project: RuntimeProject) : ProjectLifecycle(project) {
-
-        fun notifyClients() {
-            sessionMonitor.getSessionsForProject(project.name).forEach {
-                it.pages.forEach { page ->
-                    page.send("periscope-client-resource-refresh", JsonObject())
-                }
-            }
-        }
-
-        override fun onStartup(resourceIds: List<ProjectResource>) {}
-
-        override fun onShutdown(resourceIds: List<ProjectResourceId>) {}
-
-        override fun onResourcesCreated(resources: List<ProjectResource>) {
-            notifyClients()
-        }
-
-        override fun onResourcesModified(resources: List<ProjectResource>) {
-            notifyClients()
-        }
-
-        override fun onResourcesDeleted(resources: List<ProjectResourceId>) {
-            notifyClients()
-        }
+    override fun createProjectLifecycle(project: RuntimeProject): ProjectLifecycle {
+        return ClientResourceSessionNotifier(sessionMonitor, project)
     }
 }
